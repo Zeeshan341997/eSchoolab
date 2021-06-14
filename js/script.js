4f1aab1e347f61d53b257f61b37a29812a46f1ab
Zeeshan341997 <zeeshanzeegota@gmail.com>
2021-06-15 00:50:17 +0500

    ok

--- js/script.js
+++ js/script.js
@@ -2,5 +2,21 @@ $(document).ready(function(){
     $(".toggle").on('click', function(){
         $(this).toggleClass("close");
         $(".nav-toggle").toggleClass("open");
+
+        if($(".toggle").hasClass(".close")){
+            anime({
+                targets: '.toggle.close span',
+                duration: 1500,
+                rotate: [0, 45],
+                delay:500
+            });
+        }else{
+            anime({
+                targets: '.toggle span',
+                duration: 1500,
+                rotate: [45, 0],
+                delay:500
+            });
+        }
     });
 });
