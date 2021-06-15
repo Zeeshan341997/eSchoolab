3f0fe1ff3e53a8603e83dca326ba745ede906c3b
Zeeshan341997 <zeeshanzeegota@gmail.com>
2021-06-16 00:45:35 +0500

    ok

--- js/script.js
+++ js/script.js
@@ -19,4 +19,51 @@ $(document).ready(function(){
             });
         }
     });
+
+    $(".welcomeScreen, .welcomeintro").lettering();
+
+    anime({
+        targets: '.home-section .welcomeOverlay',
+        translateX: 1300,
+        delay:1000,
+        duration: 1500,
+        easing: 'easeInSine'
+    });
+    anime({
+        targets: '.home-section .welcomeImg img',
+        opacity: 1,
+        delay:1500,
+        duration: 1500,
+        easing: 'easeInSine'
+    });
+    anime({
+        targets: '.welcomeintro',
+        opacity: 1,
+        duration: 1500,
+        easing: 'easeInSine',
+        delay: anime.stagger(100, {start:3000})
+    });
+    anime({
+        targets: '.welcomeScreen',
+        opacity: 1,
+        duration: 1500,
+        easing: 'easeInSine',
+        delay: anime.stagger(100, {start:4500})
+    });
+    anime({
+        targets: '.welcomeScreen span.char4, .welcomeScreen span.char6',
+        keyframes:[
+            {translateY: 100, duration: 1500}
+        ],
+        easing: 'easeInOutExpo',
+        delay: 8000,
+    });
+    anime({
+        targets: '.welcomeScreen span.char2, .welcomeScreen span.char3, .welcomeScreen span.char5',
+        keyframes:[
+            {translateY: -100, duration: 1500}
+        ],
+        easing: 'easeInOutExpo',
+        delay: 8000,
+    });
 });
