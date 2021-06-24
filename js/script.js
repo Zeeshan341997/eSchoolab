1172c2ade5fa259846093d4618fc891ed373b244
Zeeshan341997 <zeeshanzeegota@gmail.com>
2021-06-24 22:57:18 +0500

    ok

--- js/script.js
+++ js/script.js
@@ -20,6 +20,18 @@ $(document).ready(function(){
         }
     });
 
+    const signInBtn = document.querySelector("#sign-in-btn");
+    const signUpBtn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".main-container");
+
+    signUpBtn.addEventListener('click', () => {
+        container.classList.add("sign-up-mode");
+    });
+
+    signInBtn.addEventListener('click', () => {
+        container.classList.remove("sign-up-mode");
+    });
+
     $(".welcomeScreen, .welcomeintro").lettering();
 
     anime({
@@ -74,3 +86,4 @@ function loginForm(){
     var formPopup = document.getElementById('formPopup');
     formPopup.classList.toggle('formActive');
 }
+
