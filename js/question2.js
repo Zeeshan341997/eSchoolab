d3219812b4d3864b96b662d7bab251d74c6a47e9
Zeeshan Memon <zeeshanzeegota@gmail.com>
2021-09-09 22:15:00 +0500

    ok

---
+++ js/question2.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Volume of liquid is ________  to the height of liquid",
+    answer: "proportional",
+    options: [
+      "balanced",
+      "unequal",
+      "parallel",
+      "proportional"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The property of a body to regain its original shape when deforming force have been removed called ________",
+    answer: "elasticity",
+    options: [
+      "strain",
+      "elasticity",
+      "tension",
+      "attraction"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Vernier is on the _______ of main scale.",
+    answer: "left",
+    options: [
+      "right",
+      "up",
+      "down",
+      "left"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Magnet attracts other magnetic substance with a __________",
+    answer: "force",
+    options: [
+      "pressure",
+      "force",
+      "push",
+      "magnetic line"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is called the center of gravity?",
+    answer: "point at which weight of a body act",
+    options: [
+      "acceleration",
+      "mass of surface",
+      "unparallel force",
+      "point at which weight of a body act"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Turning back of light from the boundary of the medium is called ________.",
+    answer: "reflection",
+    options: [
+      "reflection",
+      "refraction",
+      "deviation",
+      "bulb"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A prism is a piece of transparent medium bounded by ___ triangular and ____ rectangular surfaces.",
+    answer: "two, three",
+    options: [
+      "four, one",
+      "six, two",
+      "two, three",
+      "three, five"
+    ]
+  },
+  {
+    numb: 8,
+    question: "A spherical mirror is polished such that its reflecting surface is facing towards the center of sphere is called ________",
+    answer: "concave",
+    options: [
+      "convex",
+      "plain",
+      "double sided",
+      "concave"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
