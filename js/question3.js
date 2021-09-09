fbf3cb7039448f28888cc65a3ad83a5a3fcc6406
Zeeshan Memon <zeeshanzeegota@gmail.com>
2021-09-09 20:10:33 +0500

    ok

---
+++ js/question3.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Soluble in water can be seperated by?",
+    answer: "Evaporation",
+    options: [
+      "Filteration",
+      "Evaporation",
+      "Magnetic Bar",
+      "Distillation"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Solid becomes a liquid at standard atmospheric pressure is called _________",
+    answer: "Melting Point",
+    options: [
+      "Boiling Point",
+      "Melting Point",
+      "Attraction b/w particles",
+      "Temperature"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Temperature at which the vapour pressure of liquid becomes equal to the atmosphere pressure is called ________",
+    answer: "Boiling Point",
+    options: [
+      "Gas",
+      "Melting Point",
+      "Boiling Point",
+      "Liquid"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Solubility is the amount of a substance dissolve in _______ grams of a solvent (water) at a particular temperature?",
+    answer: "100",
+    options: [
+      "50",
+      "100",
+      "300",
+      "500"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Dilute hydrochloric acid  on calcium carbonate marble chips (CaCO3) prepare ________?",
+    answer: "Carbondioxide Gas",
+    options: [
+      "Carbondioxide Gas",
+      "Hydrogen",
+      "Nitrogen",
+      "Helium"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Decomposition of hydrogen peroxide (H2O2) in the presence of a catalyst manganese (MnO2) prepare _______",
+    answer: "Oxygen",
+    options: [
+      "Water Vapour",
+      "Argon",
+      "Acetylene",
+      "Propane"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Ammonia gas can be prepared by heating a mixture of _________ & _________",
+    answer: "Ammonium Chloride, Calcium Hydroxide",
+    options: [
+      "Nitrogen Dioxide, Propane",
+      "Acetylene, Argone",
+      "Ammonium Chloride, Calcium Hydroxide",
+      "Helium, Hydrogen"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Concentrated sulphuric acid on sodium chloride prepares _________",
+    answer: "Hydrochloride acid gas",
+    options: [
+      "Nitrogen Dioxide",
+      "Carbondioxide",
+      "Hydrochloride acid gas",
+      "Nitric Acid"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Passing of electric current through an electrolytic solution ions towards their respective electrodes is called _____",
+    answer: "Electrolysis",
+    options: [
+      "Mass plating",
+      "Continous plating",
+      "Rack plating",
+      "Electrolysis"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
